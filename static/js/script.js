7cbc101ddb8f6e92da13acc8abd277211851c0f2
soheylbokaie <soheyl@gmail.com>
2023-09-03 17:05:59 +0300

    html

---
+++ static/js/script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the video element
+    const video = document.getElementById("video");
+
+    // Check if the browser supports getUserMedia
+    var socket = io.connect("https://" + document.domain + ":" + location.port);
+    socket.connect();
+    const imgElement = document.getElementById("image-element");
+    socket.on("data", function(data) {
+        imgElement.src = 'data:image/png;base64, ' + data
+        console.log(data);
+
+
+    });
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        // Access the camera
+        navigator.mediaDevices
+            .getUserMedia({
+                video: true,
+            })
+            .then((stream) => {
+                video.srcObject = stream;
+            })
+            .catch((error) => {
+                console.error("Error accessing the camera:", error);
+            });
+
+        var canvas = document.createElement("canvas")
+        canvas.width = 400; // Set the desired width
+        canvas.height = 300; // Set the desired height
+        const context = canvas.getContext("2d");
+
+        function captureAndSend() {
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            const frameDataUrl = canvas.toDataURL("image/jpeg");
+            socket.emit("get_image", frameDataUrl);
+            // Call the function recursively to continuously capture and send frames
+            requestAnimationFrame(captureAndSend);
+        }
+
+        // Start capturing and sending frames when the video is playing
+        video.onplay = () => {
+            captureAndSend();
+        };
+    } else {
+        console.error("getUserMedia is not supported in this browser");
+    }
+});
